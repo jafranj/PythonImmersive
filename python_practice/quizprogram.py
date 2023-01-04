903cb8ecf6643137a562a24dc3760b07d77fb972
jafranj <63927483+jafranj@users.noreply.github.com>
2023-01-04 14:50:40 +0530

    check the program

---
+++ python_practice/quizprogram.py
@@ -0,0 +1,49 @@
+# creare a dict with questions and ans
+# variable that tracks the score
+# loop through the dict
+# display each ques to the user and let them answer
+# let them know if the ans is correct or not
+# display the final result when complete
+
+quiz = {
+    "question1": {
+        "question": "What is the capital of Sri Lanka?",
+        "answer": "Sri Jayawardane Pura"
+    },
+    "question2": {
+        "question": "What is the capital of Mali?",
+        "answer": "Timbuktu"
+    },
+    "question3": {
+        "question": "What is the capital of USA?",
+        "answer": "Wahington DC"
+    },
+    "question4": {
+        "question": "What is the capital of India?",
+        "answer": "Delhi"
+    },
+    "question5": {
+        "question": "What is the capital of Qatar?",
+        "answer": "Doha"
+    },
+    "question6": {
+        "question": "What is the capital of Morocco?",
+        "answer": "Rabat"
+    },
+}
+
+score = 0
+
+for key, value in quiz.items():
+    print(value['question'])
+    answer = input("Answer? ")
+
+    if answer.lower() == value['answer'].lower():
+        print('Correct' + '\n')
+        score += 1
+        print("Your score is: " + str(score) + '\n')
+
+    else:
+        print('Wrong' + '\n')
+        print('The Answer is: ' + value["answer"] + '\n')
+        print("Your score is: " + str(score) + '\n')
